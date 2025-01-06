6e1058be18a4401aaf15d6e8d33e0a5988ac7be5
ghen-git <gabriele.garigliano@gmail.com>
2025-01-06 15:50:00 +0100

    try at implementing cubic beziers surface definition from a curve, given up because of cuspids

--- src/shaders/2d_cubic_beziers/fragment.ts
+++ src/shaders/2d_cubic_beziers/fragment.ts
@@ -18,35 +18,47 @@ float distance_derivative(float t, vec2 p, float[8] coeffs);
 float distance_derivative_derivative(float t, vec2 p, float[8] coeffs);
 vec2 point_on_cubic(float t, float[8] coeffs);
 float[8] compute_coeffs(vec2 a, vec2 b, vec2 c, vec2 d);
-float min_distance_newton(vec2 p, vec2 a, vec2 b, vec2 c, vec2 d); 
+float min_distance_newton(vec2 p, vec2 a, vec2 b, vec2 c, vec2 d, out vec2 min_root_pos, out float min_root); 
 
 void main() {
     float[8] coeffs = compute_coeffs(a, b, c, d);
 
-    if(distance(pos, a) < 0.005)
+    if(distance(pos, a) < 5.0)
         fragColor = vec4(0.5);
-    else if(distance(pos, b) < 0.005)
+    else if(distance(pos, b) < 5.0)
         fragColor = vec4(0.5);
-    else if(distance(pos, c) < 0.005)
+    else if(distance(pos, c) < 5.0)
         fragColor = vec4(0.5);
-    else if(distance(pos, d) < 0.005)
+    else if(distance(pos, d) < 5.0)
         fragColor = vec4(0.5);
-    else
-        fragColor = vec4(min_distance_newton(pos, a, b, c, d));
+    else {
+        vec2 min_root_pos;
+        float min_root;
+
+        if(min_distance_newton(pos, a, b, c, d, min_root_pos, min_root) < 5.0) 
+            fragColor = vec4(1);
+        else{
+            vec2 ray = normalize(pos - min_root_pos);
+            vec2 tangent = normalize(point_on_cubic(min_root + 0.1, coeffs) - min_root_pos);
+            fragColor = vec4(sign(dot(tangent, ray)), 0, 0, 1);
+        }
+    }
 
 }
 
 float[8] compute_coeffs(vec2 a, vec2 b, vec2 c, vec2 d)
 {
+    float a1_x = d.x - 3.0*c.x + 3.0*b.x - a.x;
+    float b1_x = 3.0*c.x - 6.0*b.x + 3.0*a.x;
+    float c1_x = 3.0*b.x - 3.0*a.x;
+    float d1_x = a.x;
+    float a1_y = d.y - 3.0*c.y + 3.0*b.y - a.y;
+    float b1_y = 3.0*c.y - 6.0*b.y + 3.0*a.y;
+    float c1_y = 3.0*b.y - 3.0*a.y;
+    float d1_y = a.y;
+
     float coeffs[8] = float[8](
-        d.x - 3.0*c.x + 3.0*b.x - a.x,
-        3.0*c.x - 6.0*b.x + 3.0*a.x,
-        3.0*b.x - 3.0*a.x,
-        a.x,
-        d.y - 3.0*c.y + 3.0*b.y - a.y,
-        3.0*c.y - 6.0*b.y + 3.0*a.y,
-        3.0*b.y - 3.0*a.y,
-        a.y
+        a1_x, b1_x, c1_x, d1_x, a1_y, b1_y, c1_y, d1_y
     );
 
     return coeffs;
@@ -56,33 +68,55 @@ float[8] compute_coeffs(vec2 a, vec2 b, vec2 c, vec2 d)
 // check for sign changes beteween the intervals
 // in the intervals where a sign change occours, use netwtons method
 
-float min_distance_newton(vec2 p, vec2 a, vec2 b, vec2 c, vec2 d) {
-    float[8] coeffs = compute_coeffs(a, b, c, d);
+float min_distance_newton(vec2 p, vec2 a, vec2 b, vec2 c, vec2 d, out vec2 min_root_pos, out float min_root) {
+    float[8] coeffs = compute_coeffs(a, b, c, d);   
 
-    int n_roots = 1;
-    float[1] roots = float[1](0.5);
+    int n_roots = 7;
+    float[7] roots = float[7](0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0);
 
-    int steps = 5;
+    int steps = 7;
 
     for(int i = 0; i < steps; i++) {
         for(int j = 0; j < n_roots; j++) {
             float der_root = distance_derivative_derivative(roots[j], p, coeffs);
-            roots[j] = roots[j] - distance_derivative(roots[j], p, coeffs)/der_root;
+            if(abs(der_root) > 1e-6)
+                roots[j] = roots[j] - distance_derivative(roots[j], p, coeffs)/der_root;
         }
     }
 
-    float min_root = distance_function(roots[0], p, coeffs);
+    float min_distance = distance_function(roots[0], p, coeffs);
+    min_root = roots[0];
+    if(roots[0] < 0.0 || roots[0] > 1.0) {
+        min_distance = 100000.0;
+    }
+    float distance_buffer = min_distance;
+
         
     for(int j = 1; j < n_roots; j++) {
-        roots[j] = distance_function(roots[j], p, coeffs);
-        if(roots[j] < min_root)
-            min_root = roots[j];
+        if(roots[j] >= 0.0 && roots[j] <= 1.0) {
+            distance_buffer = distance_function(roots[j], p, coeffs);
+            if(distance_buffer < min_distance) {
+                min_distance = distance_buffer;
+                min_root = roots[j];
+            }
+        }
     }
 
     float dist_start = distance_function(0.0, p, coeffs);
     float dist_end = distance_function(1.0, p, coeffs);
 
-    return min_root;
+    if(dist_start < min_distance) {
+        min_distance = dist_start;
+        min_root_pos = a;
+    }
+    else if(dist_end < min_distance) {
+        min_distance = dist_end;
+        min_root_pos = d;
+    }
+    else 
+        min_root_pos = point_on_cubic(min_root, coeffs);
+
+    return min_distance;
 }
 
 // the function that outputs the distance between a point p
@@ -93,6 +127,8 @@ float distance_function(float t, vec2 p, float[8] coeffs) {
     
     float fx_t = coeffs[0]*t_3 + coeffs[1]*t_2 + coeffs[2]*t + coeffs[3];
     float fy_t = coeffs[4]*t_3 + coeffs[5]*t_2 + coeffs[6]*t + coeffs[7];
+
+    float x_component = (fx_t - p.x)*(fx_t - p.x);
     
     return (fx_t - p.x)*(fx_t - p.x) + (fy_t - p.y)*(fy_t - p.y);
 }
@@ -108,8 +144,11 @@ float distance_derivative(float t, vec2 p, float[8] coeffs) {
 
     float fy_t = coeffs[4]*t_3 + coeffs[5]*t_2 + coeffs[6]*t + coeffs[7];
     float f1y_t = 3.0*coeffs[4]*t_2 + 2.0*coeffs[5]*t + coeffs[6];
+
+    float x_component = 2.0 * (fx_t - p.x) * f1x_t;
+    float y_component = 2.0 * (fy_t - p.y) * f1y_t;
     
-    return (2.0 * (fx_t - p.x) * f1x_t) + (2.0 * (fy_t - p.y) * f1y_t);
+    return x_component + y_component;
 }
 
 // the derivative of the derivative of the function that outputs the distance between a point p
@@ -126,8 +165,8 @@ float distance_derivative_derivative(float t, vec2 p, float[8] coeffs) {
     float f1y_t = 3.0*coeffs[4]*t_2 + 2.0*coeffs[5]*t + coeffs[6];
     float f2y_t = 6.0*coeffs[4]*t + 2.0*coeffs[5];
 
-    float x_component = 2.0 * (f1x_t * f2x_t + f1x_t * f1x_t);
-    float y_component = 2.0 * (f1y_t * f2y_t + f1y_t * f1y_t);
+    float x_component = 2.0 * ((fx_t - p.x) * f2x_t + f1x_t * f1x_t);
+    float y_component = 2.0 * ((fy_t - p.y) * f2y_t + f1y_t * f1y_t);
     
     return x_component + y_component;
 }
